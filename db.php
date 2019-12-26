323d752c8b6b84663d6f8e31ecf46bd4a8d0fec2
Alan Wang <alan9130314@gmail.com>
2019-12-26 18:28:28 +0800

    add todolist file and remote

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$db=[
+'host' => 'localhost',
+'port' => 3309,
+'dbname' => 'todolist_demo',
+'username' => 'alan9130314_demo',
+'password' => 'zxc9130314',
+'charset' => 'utf8',
+];
---
+++ public_html/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Todo list demo</title>
+	
+	<link rel="stylesheet" href="css/jquery-ui.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	
+	<link rel="stylesheet" href="css/main.css">
+
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/handlebars-v4.4.3.js"></script>
+
+	<script src="js/action.js"></script>
+</head>
+<body>
